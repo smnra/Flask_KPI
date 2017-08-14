fe6d78faff9f084728f83cfe743aeb4b53834dba
smnra <hppall@163.com>
2017-08-14 16:30:30 +0800

    "2017年8月14日 16:30:23"

---
+++ template.py
@@ -0,0 +1,36 @@
+#coding=utf-8
+import flask
+from flask import Flask,render_template,request
+import user_class
+
+app = Flask(__name__)
+@app.route('/<content>')
+def index(content):
+    #content = content
+    return render_template('template_1.html',content = content)   #第一个content 模版template_1.html中的{{content}},第二个content为函数index(content)中的content 即'/<content>' 中的content
+
+@app.route('/user')
+def user():
+    user = user_class.User('SMnRa',12)
+    inputid = request.args.get('id')
+    print(type(inputid))
+    print(type(user.id))
+    if int(inputid) == 12:
+        return render_template('user_index.html',username = user.name,userid = user.id)
+    else:
+        return render_template('template_1.html',content = "Not Found user id, and id is %s" % inputid)
+
+
+
+
+@app.route('/select_user')
+def sel_user():
+    user = None
+    inputid = int(request.args.get('id'))
+    if inputid == 12:
+        user = user_class.User('SMnRa',12)
+    return render_template('user_index_1.html',user = user)
+
+
+if __name__ == '__main__':
+    app.run()
--- untitled.py
+++ untitled.py
@@ -1,10 +1,12 @@
+# coding=utf-8
 from flask import Flask
+from flask import request,url_for
 import pandas as pd
 from sqlalchemy import create_engine
-
+import flask
 word = ""
 
-sql = "select * from wcdma_day where 地市 <>'其他'  AND   日期>= '20170731' AND 日期 <  '20170901'"
+sql = "select * from wcdma_day where 地市 <>'其他'  AND   日期>= '20170731' AND 日期 <  '20170801'"
 engine = create_engine('mysql+pymysql://root:10300@192.168.3.74:50014/3g_kpi_browsing?charset=utf8')
 #df.to_sql('tick_data',engine,if_exists='append')#存入数据库，这句有时候运行一次报错，运行第二次就不报错了，不知道为什么
 df1 = pd.read_sql(sql,engine)    #read_sql直接返回一个DataFrame对象      设置多个index，只要将index_col的值设置为列表
@@ -12,9 +14,26 @@ df1 = pd.read_sql(sql,engine)    #read_sql直接返回一个DataFrame对象     
 app = Flask(__name__)
 
 
-@app.route('/index.html')
+@app.route('/')
 def index():
     return str(df1)
 
+@app.route('/user/<id>')            #获取url中的一部分
+def user(id):
+    return '<H1>Hello,%s!</H1>' % id
+
+@app.route('/query_user')           #url 键值对 获取
+def query_user():
+    id = request.args.get('id')
+    type = request.args.get('type')
+    return '<H2>query_user:%s!</H2> <h1>type is %s</h1>' %(id ,type)
+
+@app.route('/query_url/<fun>')            #反响路由查询  根据url中给出的fun 函数名 查询 相应的url
+def query_url(fun):
+    url = url_for(fun)
+    return '<h2>The Function: %s </h2> <h2> The URL is: %s</h2>' %(fun,url)
+
+
+
 if __name__ == '__main__':
-    app.run(debug= True)
+    app.run()
