52e2237237f68bc0e9dbe8eba4ccf0407394f14b
smnra <hppall@163.com>
2017-08-17 14:27:27 +0800

    "login 功能"

--- login.py
+++ login.py
@@ -5,25 +5,19 @@ from flask import render_template,url_for,request
 
 app = Flask(__name__)
 
-@app.route('/login',methods = ['POST','GET'])
+@app.route('/login',methods = ['GET','POST'])
 def login():
-    username = request.form['username']
-    password = request.form['password']
-
-    print(username)
-    print(password)
-    if not username:
-        return render_template('login.html',username = '请输入用户名!')
-    elif not password:
-        return render_template('login.html',password = '请输入用户名!')
-    elif username is 'SMnRa' and password is '123456':
-        return render_template('main.html',username = username)
-    else:
-        return render_template('login.html',message = '用户名或密码错误!')
-
+    if request.method =='POST':
+        username = request.form['username']
+        password = request.form['password']
+        if username == 'SMnRa' and password == '123456':
+            return render_template('user.html',username = username)
+        else:
+            return render_template('login.html',message = '用户名或密码错误!')
+    return render_template('login.html',message = '用户名或密码错误!')
 
 
 
 
 if __name__ == '__main__':
-    app.run('127.0.0.1',port = 80)
+    app.run()
